8b60f3210010c27a03833f2898825cd8c97fe03c
NeguliSimanis <simanism@inbox.lv>
2018-01-27 10:44:10 +0200

    Finished story intro

--- Assets/Scripts/MoveUI.cs
+++ Assets/Scripts/MoveUI.cs
@@ -18,10 +18,41 @@ public class MoveUI : MonoBehaviour {
     RectTransform story1;
     private bool isStory1Moving = false;
 
+    [SerializeField]
+    private GameObject storyUI2;
+    RectTransform story2;
+    private bool isStory2Moving = false;
+
+    [SerializeField]
+    private GameObject storyUI3;
+    RectTransform story3;
+    private bool isStory3Moving = false;
+
+    [SerializeField]
+    private GameObject storyUI4;
+    RectTransform story4;
+    private bool isStory4Moving = false;
+
+    [SerializeField]
+    private GameObject storyUI5;
+    RectTransform story5;
+    private bool isStory5Moving = false;
+
+    [SerializeField]
+    private GameObject storyUI6;
+    RectTransform story6;
+    private bool isStory6Moving = false;
+
     void Start()
     {
         UItoMove = gameObject.GetComponent<RectTransform>();
         story1 = storyUI1.GetComponent<RectTransform>();
+        story2 = storyUI2.GetComponent<RectTransform>();
+        story3 = storyUI3.GetComponent<RectTransform>();
+        story4 = storyUI4.GetComponent<RectTransform>();
+        story5 = storyUI5.GetComponent<RectTransform>();
+        story6 = storyUI6.GetComponent<RectTransform>();
+
         canvas = GameObject.Find("Canvas").GetComponent<RectTransform>();
 
         startingPosition = transform.position;
@@ -50,9 +81,68 @@ public class MoveUI : MonoBehaviour {
         if (isStory1Moving)
         {
             story1.transform.Translate(speed, 0f, 0f);
+            
         }
 
-        
-        //transform.position = new Vector3(startingPosition.x, canvas.rect.height + UItoMove.rect.height, startingPosition.z);
+        if (story1.position.x < distanceToMove)
+        {
+            isStory1Moving = false;
+            isStory2Moving = true;
+        }
+
+        if (isStory2Moving)
+        {
+            story2.transform.Translate(speed, 0f, 0f);
+        }
+
+        if (story2.position.x < distanceToMove)
+        {
+            isStory2Moving = false;
+            isStory3Moving = true;
+        }
+
+        if (isStory3Moving)
+        {
+            story3.transform.Translate(speed * 2, 0f, 0f);
+        }
+
+        if (story3.position.x < distanceToMove)
+        {
+            isStory3Moving = false;
+            isStory4Moving = true;
+        }
+
+        if (isStory4Moving)
+        {
+            story4.transform.Translate(speed * 2, 0f, 0f);
+        }
+
+        if (story4.position.x < distanceToMove)
+        {
+            isStory4Moving = false;
+            isStory5Moving = true;
+        }
+
+        if (isStory5Moving)
+        {
+            story5.transform.Translate(speed * 2, 0f, 0f);
+        }
+
+        if (story5.position.x < distanceToMove)
+        {
+            isStory5Moving = false;
+            isStory6Moving = true;
+        }
+
+        if (isStory6Moving)
+        {
+            story6.transform.Translate(0f, -speed, 0f);
+        }
+
+        if (story6.position.y > distanceToMove)
+        {
+            isStory6Moving = false;
+        }
+
     }
 }
