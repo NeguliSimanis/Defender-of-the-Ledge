4493932a8672c5fa4af832c7b4fc7d196e6fa88b
NeguliSimanis <simanism@inbox.lv>
2018-01-27 23:49:10 +0200

    Started working on boss arena

---
+++ Assets/Scripts/BossZoneStart.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossZoneStart : MonoBehaviour
+{
+    [SerializeField]
+    GameObject audioCamera;
+
+    [SerializeField]
+    AudioClip bossMusic;
+
+    private bool isBossActive = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!isBossActive && other.gameObject.tag == "Player")
+        {
+            AudioSource audio = audioCamera.GetComponent<AudioSource>();
+            audio.clip = bossMusic;
+            audio.Play();
+            isBossActive = true;
+        }
+    }
+}
+
