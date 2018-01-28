be2ec0a8c2b88c98417fb0380a822ccb305c1dc6
NeguliSimanis <simanism@inbox.lv>
2018-01-28 04:00:30 +0200

    Added healing totem

---
+++ Assets/Scripts/Totem.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Totem : MonoBehaviour {
+
+    PlayerData playerData;
+    Animator _animator;
+
+    public bool isPlayerNear = false;
+    private bool isHealAvailable = true;
+
+    [SerializeField]
+    private int healAmount = 30;
+    [SerializeField]
+    private float healCooldown = 5f;
+    private float nextHealTime;
+
+    void Start ()
+    {
+        _animator = gameObject.GetComponent<Animator>();
+        playerData = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerData>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            isPlayerNear = true;
+            _animator.SetBool("signal", true);
+        }
+        //Debug.Log("player near");
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            isPlayerNear = false;
+            _animator.SetBool("signal", false);
+        }
+           // Debug.Log("player far");
+    }
+    public void HealPlayer()
+    {
+        if (playerData.hp < playerData.maxHp)
+        {
+            if (isHealAvailable)
+            {
+                playerData.hp = playerData.hp + healAmount;
+                nextHealTime = Time.time + healCooldown;
+                _animator.SetBool("inactive", true);
+                isHealAvailable = false;
+            }   
+        }
+    }
+
+    void Update()
+    {
+        if (!isHealAvailable && nextHealTime <= Time.time)
+        {
+            isHealAvailable = true;
+            _animator.SetBool("inactive", false);
+        }
+    }
+}
---
+++ Assets/Scripts/TotemHeal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TotemHeal : MonoBehaviour {
+
+    [SerializeField]
+    GameObject parentTotem;
+    Totem totem;
+
+    void Start()
+    {
+        totem = parentTotem.GetComponent<Totem>();
+    }
+
+    void OnMouseDown()
+    {
+        if (totem.isPlayerNear)
+        {
+            totem.HealPlayer();
+            Debug.Log("heal called ");
+        }
+    }
+}
